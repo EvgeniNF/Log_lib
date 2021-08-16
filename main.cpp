a01bf4074abfba11b7f9d85ace14d9c40b0da5d9
EvgeniNF <karpenkojenia28@gmail.com>
2021-08-16 22:46:13 +0300

    Testing work whith files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <memory>
 #include <loglib.hpp>
+#include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 int main() {
     auto a = std::make_shared<Logs>("path");
-    std::string file = "test.txt";
+    auto current_path = std::make_unique<std::filesystem::path>(std::filesystem::current_path());
+    std::string path = current_path->string();
+    char slash = 0x5C;
+    path += slash;
+    path += "test.txt";
+    std::cout << path << std::endl;
     std::ofstream out;
-    out.open(file);
+    out.open(path);
     out << "Hello, logs";
     out.close();
     return 0;
